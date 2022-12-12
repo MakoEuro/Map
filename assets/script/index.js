a45a4fa264c7d45c9476932bd9d32d080b774178
Mako <starykovskyeric@gmail.com>
2022-12-12 17:10:15 -0600

    added background piece and finished up project, added map function along with slight responsiveness

--- assets/script/index.js
+++ assets/script/index.js
@@ -19,13 +19,33 @@ function sleep(duration) {
 const mapFunc = select('#map');
 
 // - - - - - - - - - - - - -
+
 // Retrieve location
-function getLocation(position) {
-    const { latitude, longitude } = position.coords; 
+function getLocation() {
+    var lon;
+    var lat;
 
-    return latitude, longitude;
+    var locPromise = new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(function(pos){
+            lon = pos.coords.longitude
+            lat = pos.coords.latitude;
+            resolve({lon,lat});
+        });
+    });
+     
+    locPromise.then(function(value) {
+        workMap.flyTo ({
+            center: [value.lon, value.lat],
+            zoom: 10,
+            showUserHeading: true
+        }) 
+
+        const marker = new mapboxgl.Marker()
+            .setLngLat([value.lon, value.lat])
+            .addTo(workMap);
+    });
 }
-
+ 
 // Fail
 function errorHandler(error) {
     console.log(error.message);
@@ -39,27 +59,16 @@ window.onload = function() {
     if (navigator.geolocation) {
         const geo = navigator.geolocation;
         geo.getCurrentPosition(getLocation, errorHandler, options);
+        getLocation();    
     } else {
         console.log('Geolocation is not supported by your browser');
     }
 }
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFrb2V1cm8iLCJhIjoiY2xibDAyNGZ5MDJuYTN2cXg1N3ZscThjdyJ9.gMpm_x3YDzyA-wCpJASBNw';
-const map = new mapboxgl.Map({
+const workMap = new mapboxgl.Map ({
     container: mapFunc, // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [getLocation()], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 3 // starting zoom
 });
 
-map.on('load', () => {
-    map.addLayer({
-        id: 'terrain-data',
-        type: 'line',
-        source: {
-            type: 'vector',
-            url: 'mapbox://mapbox.mapbox-terrain-v2'
-        },
-        'source-layer': 'contour'
-    });
-});
